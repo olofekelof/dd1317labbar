15779e37590e680a9ead01f6f3dceae9110e9b85
olofekelof <145376188+olofekelof@users.noreply.github.com>
2023-09-27 16:20:15 +0200

    lägger till input funktion

--- Labb 1/oloflabb.py
+++ Labb 1/oloflabb.py
@@ -1,5 +1,5 @@
 """
-Det här är olof labb 1
+Det här är olof labb 1 dd1317
 """
 def aritmetisk_talfoljdsberaknare(n, A1, d):
     """
@@ -30,7 +30,7 @@ def geometrisk_talfoljdsberaknare(n, A1, q):
 FIRST_ELEMENT = int(input("Vad är det första talet i din talföljd?")) #första talet i din talföljd
 DIFFERENS = int(input("Vad är differesen mellan talen i din talföljd")) #differensen mellan talen i din talföljd
 NUMBER_OF_ELEMENTS = int(input("Hur många tal är det i din talföljd?")) #antalet tal i din talföljd
-QUOTA = int(input("Vad är kvoten av de två på varandra följande talen i din talföljd?"))#kvoten av två på varandra följande tal i talföljden
+QUOTA = int(input("Vad är kvoten av de två på varandra följande talen i din talföljd?")) #kvoten av två på varandra följande tal i talföljden
 
 print(f"Den aritmetiska talföljden är {aritmetisk_talfoljdsberaknare(NUMBER_OF_ELEMENTS, FIRST_ELEMENT, DIFFERENS)}") #skriver ut summan av den aritmetiska talföljden
 print(f"Den geometriska talföljden är {geometrisk_talfoljdsberaknare(NUMBER_OF_ELEMENTS, FIRST_ELEMENT, QUOTA)}") #skriver ut summan av den geometriska talföljden
